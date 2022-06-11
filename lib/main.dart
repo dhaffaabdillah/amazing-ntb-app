356f49d209ba911dcf92adec1b75697ee2d54bcc
dhaffaabdillah <dhaffdhaff1@gmail.com>
2022-06-11 17:58:37 +0700

    error handshake certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:travel_hour/services/http_overrides.dart';
 import 'app.dart';
 
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
   await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
